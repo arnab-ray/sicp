2a1b7b1b9bd6b51dead700630e40f9d875bb6ffa
Arnab Ray <arnab.ray@udaan.com>
2023-08-22 22:13:14 +0530

    save solutions

--- ch2/ex2.1.rkt
+++ ch2/ex2.1.rkt
@@ -121,7 +121,91 @@
 (define (lower-bound i) (min (car i) (cdr i)))
 (define (upper-bound i) (max (car i) (cdr i)))
 
+
+;Helper
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
 ; Problem 2.8
 (define (sub-interval x y)
   (make-interval (- (lower-bound x) (upper-bound y))
                  (- (upper-bound x) (lower-bound y))))
+
+; Problem 2.10
+(define (div-interval x y)
+  (if (<= 0 (* (lower-bound y) (upper-bound y)))
+  (error "Interval spans zero")
+  (mul-interval x 
+                (make-interval (/ 1.0 (upper-bound y))
+                               (/ 1.0 (lower-bound y))))))
+
+; Problem 2.12
+(define (make-center-percent c p)
+  (let ([w (* c (/ p 100.0))])
+    (make-interval (- c w) (+ c w))))
+
+(define (percent i)
+  (let ([c (/ (+ (lower-bound i) (upper-bound i)) 2.0)]
+        [w (/ (- (upper-bound i) (lower-bound i)) 2.0)])
+    (* (/ w c) 100)))
+
+; Problem 2.17
+(define (last-pair l)
+  (cond [(null? l) null]
+        [(null? (cdr l)) (car l)]
+        [else (last-pair (cdr l))]))
+
+
+; Problem 2.18
+(define (reverse l)
+  (cond [(null? l) null]
+        [(null? (cdr l)) l]
+        [else (append (reverse (cdr l)) (list (car l)))]))
+
+; Problem 2.19
+(define (first-denomination l) (car l))
+
+(define (except-first-denomination l) (cdr l))
+
+(define (no-more? l) (null? l))
+
+; Problem 2.20
+(define (same-parity f . r)
+  (define (iter l elements target-remainder)
+    (if (null? l)
+        elements
+        (iter (cdr l)
+              (if (= (remainder (car l) 2) target-remainder)
+                  (append elements (list (car l)))
+                  elements)
+              target-remainder)))
+  (iter r (list f) (remainder f 2)))
+
+; Problem 2.21
+(define (square-list items)
+  (if (null? items)
+      null
+      (cons (* (car items) (car items)) (square-list (cdr items)))))
+
+; Helper
+(define (map proc items)
+  (if (null? items)
+      null
+      (cons (proc (car items))
+            (map proc (cdr items)))))
+
+(define (square-list-map items)
+  (map (lambda (x) (* x x)) items))
+
+; Problem 2.23
+(define (for-each proc items)
+  (cond [(null? items) (newline)]
+        [else (proc (car items)) (for-each proc (cdr items))]))
+
+; Problem 2.26
+(define x (list 1 2 3))
+(define y (list 4 5 6))
